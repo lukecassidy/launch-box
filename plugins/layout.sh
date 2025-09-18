6cdf55f68b5a83fa3a80bc4bb249f89f20479d77
Luke Cassidy <lukecassidy88@gmail.com>
2025-09-19 00:21:05 +0100

    Update layout.sh

--- plugins/layout.sh
+++ plugins/layout.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source "$(dirname "${BASH_SOURCE[0]}")/../lib/common.sh"
-echo "Applying window layout via Hammerspoon..."
+log INFO "Applying window layout via Hammerspoon..."
 hs -c "applyWorkspace()"
