810616e003e9fa069dc8495151a02741fc078f49
Luke Cassidy <lukecassidy88@gmail.com>
2025-09-19 00:21:30 +0100

    Update iTerm.sh

--- plugins/iTerm.sh
+++ plugins/iTerm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source "$(dirname "${BASH_SOURCE[0]}")/../lib/common.sh"
-echo 'iTerm configuration script running...'
+log INFO 'iTerm configuration script running...'
 
 # Create two horizontal panes and run test commands
 osascript <<'EOF'
