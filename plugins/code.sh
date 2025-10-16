ba6c39a392cac418638be589cc74c5b0cae7c5ca
Luke Cassidy <lukecassidy88@gmail.com>
2025-10-16 23:34:22 +0100

    More graceful merging of vscode windows

--- plugins/code.sh
+++ plugins/code.sh
@@ -10,14 +10,24 @@ log INFO "Merging all Visual Studio Code 'mac' windows..."
 # Merge all Visual Studio Code windows
 osascript <<'EOF'
 tell application "System Events"
-    tell process "Code"
-        set frontmost to true
-        try
-            click menu item "Merge All Windows" of menu "Window" of menu bar 1
-        on error
-            display notification "Couldn't merge windows. " with title "Visual Studio Code"
-        end try
-    end tell
+    -- Wait briefly if VS Code hasn't finished launching yet
+    set waited to 0
+    repeat until exists (process "Code") or (waited > 10)
+        delay 0.5
+        set waited to waited + 0.5
+    end repeat
+
+    if exists (process "Code") then
+        tell process "Code"
+            set frontmost to true
+            delay 0.3
+            try
+                click menu item "Merge All Windows" of menu "Window" of menu bar 1
+            on error
+                display notification "Couldn't merge windows." with title "Visual Studio Code"
+            end try
+        end tell
+    end if
 end tell
 EOF
 
